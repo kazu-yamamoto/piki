966319b94331d1c4fa2accb06eba00b6f94f6ed3
Kazu Yamamoto <kazu@iij.ad.jp>
2010-07-02 18:50:39 +0900

    verion 0.4.0.

--- src/Main.hs
+++ src/Main.hs
@@ -10,7 +10,7 @@ import Html
 ----------------------------------------------------------------
 
 version :: String
-version = "0.3.1"
+version = "0.4.0"
 
 printVersion :: IO ()
 printVersion = putStrLn . (++ " version " ++ version) =<< getProgName
