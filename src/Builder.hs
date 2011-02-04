930157092dfb9bc71c72c0b62f68a4c109100148
Kazu Yamamoto <kazu@iij.ad.jp>
2011-02-05 07:47:59 +0900

    using the text builder.

--- src/Builder.hs
+++ src/Builder.hs
@@ -4,27 +4,18 @@ module Builder (
   ) where
 
 import Data.Monoid
-import Data.String
 import qualified Data.Text.Lazy as L
-
-data Builder = Builder (L.Text -> L.Text)
-
-instance IsString Builder where
-    fromString s = Builder (L.pack s +++)
-
-instance Monoid Builder where
-    mempty = Builder id
-    mappend (Builder x) (Builder y) = Builder (x . y)
+import Data.Text.Lazy.Builder
 
 (+++) :: Monoid m => m -> m -> m
 (+++) = mappend
 infixr 5 +++
 
 toB :: L.Text -> Builder
-toB txt = Builder (txt `L.append`)
+toB = fromLazyText
 
 mcatmap :: (a -> Builder) -> [a] -> Builder
 mcatmap f = mconcat . map f
 
 build :: Builder -> L.Text
-build (Builder f) = f L.empty
+build = toLazyText
