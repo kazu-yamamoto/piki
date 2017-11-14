df46474868a385dc8533f4de0e3cc262a2936282
Kazu Yamamoto <kazu@iij.ad.jp>
2017-11-14 17:18:00 +0900

    Using Text instance of Parsec.

--- src/Parsec.hs
+++ src/Parsec.hs
@@ -7,10 +7,6 @@ import Control.Applicative as AP hiding (many,optional,(<|>))
 import qualified Data.Text.Lazy as L
 import Text.Parsec as AP hiding (satisfy, ParseError, errorPos)
 import Text.Parsec.Error as AP
-import Text.Parsec.Prim
 
 type Parser = Parsec L.Text ()
 type LineParser = Parsec [L.Text] ()
-
-instance Monad m => Stream L.Text m Char where
-    uncons = return . L.uncons
